728418caa49f6a3ce8018f6975e330993ce916b5
Eugene Milford <gmilford321@gmail.com>
2023-01-31 12:46:16 +0200

    Comments Added

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MinLengthValidator
+from django.db.models.fields import CharField
 
 # Create your models here.
 
@@ -29,3 +30,9 @@ class Post(models.Model):
     content = models.TextField(validators = [MinLengthValidator(10)])
     author = models.ForeignKey(Author, null=True, on_delete=models.SET_NULL, related_name= "posts")
     tags = models.ManyToManyField(Tag)
+
+class Comment(models.Model):
+    user_name = models.CharField(max_length = 120)
+    user_email = models.EmailField()
+    text = models.TextField(max_length = 300)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name = "comments")
